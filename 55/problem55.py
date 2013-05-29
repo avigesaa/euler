2b24f6b8e709e62526e820de305205b256af5033
Andrew Vigesaa <avigesaa@gmail.com>
2013-05-29 02:05:20 -0400

    Rename reverse_str to reversed_str in #55

--- 55/problem55.py
+++ 55/problem55.py
@@ -1,12 +1,12 @@
-def reverse_str(n):
+def reversed_str(n):
     return str(n)[::-1]
 
 def is_palindromic(n):
-    return str(n) == reverse_str(n)
+    return str(n) == reversed_str(n)
     
 def is_lychrel(n):
     for i in xrange(50):
-        n = n + int(reverse_str(n))
+        n = n + int(reversed_str(n))
         if is_palindromic(n):
             return False        
     return True
