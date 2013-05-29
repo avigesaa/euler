32952bf3d0ca3914d32978427eada63f8ff9a118
Andrew Vigesaa <avigesaa@gmail.com>
2013-05-29 01:06:05 -0400

    Tweak #34

--- 34/problem34.py
+++ 34/problem34.py
@@ -3,11 +3,5 @@ from math import factorial
 def factorial_digit_sum(n):
     return factorial(n % 10) + factorial_digit_sum(n // 10) if n > 0 else 0
 
-result = 0
-
-for x in xrange(10, 4999999):
-    if x == factorial_digit_sum(x):
-        result += x
-        
-print result
+print sum(x for x in xrange(10, 9999999) if x == factorial_digit_sum(x))
 
